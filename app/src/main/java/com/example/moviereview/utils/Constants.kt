b17712785c7de2fc0c15dbcf9c49a35d58fd2ae2
Tarikul Islam Shykat <77191261+Tarikul-Islam-Shykat@users.noreply.github.com>
2023-10-04 05:27:48 +0600

    Update Constants.kt

--- app/src/main/java/com/example/moviereview/utils/Constants.kt
+++ app/src/main/java/com/example/moviereview/utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.moviereview.utils
 
 object Constants {
-    const val  API_KEY = "6887e4f0b1ab01efc49fe6d77dc24149"
+    const val  API_KEY = "get your own"
     const val  BASE_URL = "https://api.themoviedb.org/3/"
     const val  POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342"
 
